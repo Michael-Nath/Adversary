d1fc12f640f5b2b2537d054ed371d48b4fb268a9
Michael <michael.d.nath@gmail.com>
2022-04-11 22:34:56 -0700

    finished question 1

--- blockUtils.ts
+++ blockUtils.ts
@@ -1,4 +1,12 @@
-import { Transaction, Block} from "./types"
-export function createObjectID(object: Block | Transaction) {
-    
+import { Transaction, Block } from "./types";
+import * as sha256 from "fast-sha256";
+
+const canonicalize = require("canonicalize");
+export function createObjectID(object: Block | Transaction): string {
+	const canonicalizedJSON = canonicalize(object);
+	const hash = sha256.hash(canonicalizedJSON);
+	return Buffer.from(hash).toString("hex");
 }
+
+const block = { type: "block", txids: [""], nonce: "", previd: "" } as Block;
+console.log(createObjectID(block));
